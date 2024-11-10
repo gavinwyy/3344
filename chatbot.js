db2353f0df0ce8d161868cb9e630feda7458c0d9
gavinwyy <68812138+gavinwyy@users.noreply.github.com>
2024-11-10 16:18:03 +1100

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyCod1o5IvCYcjavIUyLAKwaa8jhbADRJws";
+const API_KEY = ""; // API key
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
